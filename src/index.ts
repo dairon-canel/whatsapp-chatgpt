b4897791f3c5f6e27f0a8ee11a77e8d8abd9347f
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2023-01-01 14:14:32 -0500

    feat: finished logic

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,11 @@
-import process from 'process';
 import qrcode from 'qrcode-terminal';
 import { Client, LocalAuth } from 'whatsapp-web.js';
-import { ChatGPTAPIBrowser } from 'chatgpt';
-
-// Environment variables
-require('dotenv').config();
-
-// Prefix check
-const prefixEnabled = process.env.PREFIX_ENABLED == 'true';
-const prefix = '!gpt';
+import { gptResponse } from './gpt';
 
 // Whatsapp Client
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: { headless: true },
-});
-
-async function initChatGPT() {
-  const api = new ChatGPTAPIBrowser({
-    email: String(process.env.EMAIL),
-    password: String(process.env.PASSWORD),
-    isGoogleLogin: true,
-  });
-
-  await api.initSession();
-
-  return {
-    sendMessage: (message, opts = {}) => {
-      return api.sendMessage(message, opts);
-    },
-  };
-}
-
-// ChatGPT Client
-
-const api = await initChatGPT().catch(e => {
-  console.error(e);
-  process.exit();
 });
 
 // Entrypoint
@@ -52,53 +21,15 @@ const start = async () => {
     console.log('[Whatsapp ChatGPT] Client is ready!');
   });
 
-  // Whatsapp message
+  // Whatsapp message_create
   client.on('message_create', async (message: any) => {
     console.log(message.body);
     if (message.body.length == 0) return;
     if (message.from == 'status@broadcast') return;
-
-    if (prefixEnabled) {
-      if (message.body.startsWith(prefix)) {
-        // Get the rest of the message
-        const prompt = message.body.substring(prefix.length + 1);
-        await handleMessage(message, prompt);
-      }
-    } else {
-      await handleMessage(message, message.body);
-    }
+    gptResponse(client, message);
   });
 
   client.initialize();
-};
-
-const handleMessage = async (message: any, prompt: any) => {
-  try {
-    const start = Date.now();
-
-    // Send the prompt to the API
-    console.log(
-      '[Whatsapp ChatGPT] Received prompt from ' + message.from + ': ' + prompt,
-    );
-    const response = await api.sendMessage(prompt);
-
-    console.log(
-      `[Whatsapp ChatGPT] Answer to ${message.from}: ${response.response}`,
-    );
-
-    const end = Date.now() - start;
-
-    console.log('[Whatsapp ChatGPT] ChatGPT took ' + end + 'ms');
-
-    // Send the response to the chat
-    message.reply(response.response);
-  } catch (error: any) {
-    message.reply(
-      'An error occured, please contact the administrator. (' +
-        error.message +
-        ')',
-    );
-  }
 };
 
 start();
